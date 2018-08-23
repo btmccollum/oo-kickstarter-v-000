bf217eb925369319418c05114156766ad2aea76a
Brad McCollum <btmcco8@gmail.com>
2018-08-23 21:02:49 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,9 +7,12 @@ class Backer
     # binding.pry
     @name = name
     @backed_projects = []
-
-
   end
 
+  #accepts a Project as an argument and stores it in a backed projects array
+  def back_project(project)
+    # self.project.backers << self
+    @backed_projects << project
+  end
 
 end
