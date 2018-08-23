35d00236c51b5fcaebbf336d8ae4bab34f6b2e08
Brad McCollum <btmcco8@gmail.com>
2018-08-23 21:20:20 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,12 +8,12 @@ class Project
     @backers = []
   end
 
-  # def backers
-  #   @backers
-  # end
+  def backers
+    @backers
+  end
 
   #it accepts an argument of a specific instance of Backer and adds this project
-  #to that backers array of backed project if it's not already present, and then
+  #to that backers array of backed project if it's not already present, and then 
   #adds the backer to the project's @backers array
   def add_backer(backer)
     backer.back_project(self) unless backer.backed_projects.include?(self)
