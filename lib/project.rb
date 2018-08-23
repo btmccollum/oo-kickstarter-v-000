ef4c1a30cf1115e804bda7b20d153a171ecba727
Brad McCollum <btmcco8@gmail.com>
2018-08-23 20:59:50 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,11 +1,17 @@
 class Project
 
   attr_accessor :name
+  attr_reader :backers
 
   def initialize(name)
     @name = name
     @backers = []
+  end
 
+  def backers
+    @backers
+  end
+  
 
 
 end
